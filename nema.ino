217af8bf9627bc3b80f63e86ce8c71cd4ba4b49c
Martin Sanchez <tinchofiuba@gmail.com>
2025-02-26 17:00:10 -0300

    scketch básico para medir TDS, calibrado con condictimetro de analitica

--- nema.ino
+++
@@ -1,2 +0,0 @@
-#include TDS 0
-
---
+++ sensorTDS.ino
@@ -0,0 +1,21 @@
+#include TDS 0
+
+float leerTDS() {
+  int sensorValue = analogRead(TDS_PIN);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // Convertir el voltaje a TDS (esto depende de tu sensor y su calibración)
+  float tdsValue = (voltage * 1000) / 2; // Ejemplo de conversión
+  return tdsValue;
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float tds = leerTDS();
+
+  Serial.print("TDS: ");
+  Serial.print(tds);
+  Serial.println(" ppm");
+}
