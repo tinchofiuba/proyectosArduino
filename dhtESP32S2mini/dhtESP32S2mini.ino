3ba56f3ce69eccb2eec9a4f73f460fcc684c5f32
Martin <tinchofiuba@gmail.com>
2025-03-02 18:58:09 -0300

    CREO UN XLSX PARA CALIBRAR el sensor ultrasónico y preparo para medir tds. todo transmitiendo a una web

---
+++ dhtESP32S2mini/dhtESP32S2mini.ino
@@ -0,0 +1,91 @@
+#include <WiFi.h>
+#include <WebServer.h>  
+#include "DHT.h"  // Librería para el sensor DHT
+
+#define DHTPIN 3 
+#define DHTTYPE DHT22
+#define TRIG_PIN 5 
+#define ECHO_PIN 7
+#define TDS_PIN 9
+
+const char* ssid = "Telecentro-fd55";  
+const char* password = "VTWMK4AUKMRW";
+
+WebServer server(80);  
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void handleRoot() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  long distancia = leerDistancia();
+  // Si falla la lectura, muestra un mensaje de error
+  if (isnan(h) || isnan(t)) {
+    server.send(500, "text/plain", "Error al leer el sensor DHT!");
+    return;
+  }
+
+  // Página HTML con los valores en vivo
+  String html = "<html><head><meta http-equiv='refresh' content='5'></head><body>";
+  html += "<h1>ESP32-S2 - Monitoreo DHT</h1>";
+  html += "<p><b>Temperatura:</b> " + String(t,2) + "</p>";
+  html += "<p><b>Humedad:</b> " + String(h,2) + "</p>";
+  html += "<p><b>Distancia:</b> " + String(distancia) + " mm</p>";
+  html += "<p><b>Conductividad:</b> " + String(TDS) + " uS</p>";
+  html += "</body></html>";
+
+  server.send(500, "text/html", html);
+}
+
+long leerDistancia() {
+  long to=micros();
+  while(micros()-to<10)
+  {digitalWrite(TRIG_PIN, HIGH);}
+   digitalWrite(TRIG_PIN, LOW);  
+
+    // wait for echo pulse to go high
+    while(!digitalRead(ECHO_PIN)) ;  
+    // start timer
+    unsigned long StartTime = micros();
+    // wait for end of echo pulse (goes low)
+    while(digitalRead(ECHO_PIN)) ;
+    unsigned long CurrentTime = micros();
+    // calculate length of echo pulse in microseconds
+    unsigned long distancia = (CurrentTime - StartTime)*0.1911-37.21;
+  delay(1000);
+  return distancia;
+}
+
+float leerTDS() {
+  int sensorValue = analogRead(TDS_PIN);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // Convertir el voltaje a TDS (esto depende de tu sensor y su calibración)
+  float tdsValue = (voltage * 1000) / 2; // Ejemplo de conversión
+  return tdsValue;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  dht.begin();  // Iniciar DHT
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando a WiFi...");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConectado a WiFi!");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
