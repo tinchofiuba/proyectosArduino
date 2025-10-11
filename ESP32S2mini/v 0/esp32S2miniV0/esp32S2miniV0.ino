d4643ffc20493cbab297d320f974249f949e9dd1
Martin <tinchofiuba@gmail.com>
2025-10-11 17:48:45 -0300

    cambio el c3 mini por un s2mini y pruebo el codigo

--- ESP32S2mini/v 0/esp32S2miniV0/esp32S2miniV0.ino
+++ ESP32S2mini/v 0/esp32S2miniV0/esp32S2miniV0.ino
@@ -5,40 +5,45 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <ArduinoJson.h>
+#include <Wire.h>
+
+#define AS5600_ADDR 0x36  // Dirección I2C por defecto del AS5600
 
 //pines analógicos
-#define pin_ph 1 //pin del ph
-#define pin_EC 2 //pin del conductimetro
+#define PIN_PH 2 //pin del ph (ADC1_2)
+#define PIN_EC 4 //pin del conductimetro (ADC1_4)xx,,,mm    
 
 //pines ON /OFF
-#define LL 3 // tacho ppal
-#define LL 4 // tacho ppal
-#define HL 5 // tacho ppal
-#define HH 6 // tacho ppal
-#define LL_2 7 // tacho secundario
-#define HH_2 10 // tacho secundario
-#define wifi 11 // habilitación de envío de datos
+#define LL 43 // tacho ppal
+#define L 41 // tacho ppal
+#define H 39 // tacho ppal
+#define HH 37 // tacho ppal
+
+#define PIN_TRASMITIR 7 // habilitación de envío de datos
+
+int pines_on_off[] = {LL, L, H, HH, PIN_TRASMITIR};
+int n_pines_on_off = sizeof(pines_on_off) / sizeof(pines_on_off[0]);
 
 //pines periféricos I2C
 #define pin_SDA 8
 #define pin_SCL 9
 
-//pines de servo / motores por pasos
-#define pin_servo_ph 12 //servomotor para el phmetro
-
 //pines digitales sensores
-#define TRIG_PIN 13
-#define ECHO_PIN 14
-#define DS18B20 15
+#define DS18B20 13 // pin 18 no puede usar pull-up/pull-down OJO
+#define DHTPIN 11 // pin para sensor DHT22
 #define DHTTYPE DHT22
 
-//pines de bombas
-#define PIN_BOMBA 16
-#define PIN_BOMBA_A 17
-#define PIN_BOMBA_B 18
-#define PIN_BOMBA_micro 21
-#define PIN_BOMBA_FE 33
-#define PIN_BOMBA_AGUA_RESERVA 34
+//pines de bombas y servomotores y/o motores
+#define PIN_SERVO_PH 24 //servomotor para el phmetro
+#define PIN_BOMBA_PPAL 14
+#define PIN_AIREADOR 12
+#define PIN_BOMBA_A 10
+#define PIN_BOMBA_B 6
+#define PIN_BOMBA_micro 4
+#define PIN_BOMBA_FE 2
+#define PIN_BOMBA_AGUA_RESERVA 1
+
+//queda el GPIO7 libre, o bien el ADC1_6 libre
 
 
 OneWire oneWire(DS18B20);
@@ -46,7 +51,7 @@ DallasTemperature sensors(&oneWire);
 DHT dht(DHTPIN, DHTTYPE);
 
 const int numIter = 10; 
-const char* serverName = "http://tinchofiuba.pythonanywhere.com/hidroponia/";
+const char* serverName = "http://tinchofiuba.pythonanywhere.com/hidroponia12/";
 
 float tAguaArray[numIter];
 float humedadArray[numIter];
@@ -55,30 +60,86 @@ float distanciaArray[numIter];
 float conductividadArray[numIter]; // C9
 float phArray[numIter];        // C8
 
-long leerDistancia() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(5);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+// Variables de estado de las bombas
+bool bombaPpalEstado = false;
+bool bombaAEstado = false;
+bool bombaBEstado = false;
+bool bombaMicroEstado = false;
+bool bombaFEEstado = false;
+bool bombaAguaReservaEstado = false;
+bool servoMotorPhEstado = false;
 
-  long duration = pulseIn(ECHO_PIN, HIGH);
-  delay(30);
-  return duration;
+
+float leerAngulo() {
+  Wire.beginTransmission(AS5600_ADDR);
+  Wire.write(0x0C);  // Dirección del registro RAW ANGLE (MSB)
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(AS5600_ADDR, 2);
+  if (Wire.available() < 2) return -1;
+
+  uint8_t highByte = Wire.read();
+  uint8_t lowByte  = Wire.read();
+  uint16_t valor = ((highByte << 8) | lowByte) & 0x0FFF;  // 12 bits
+
+  // Convertir a grados
+  float angulo = (valor * 360.0) / 4096.0;
+  if (angulo > 0 )
+  {
+  return angulo;
+  }
+  else
+  {
+    return 3.1416; //error de lectura I2C
+  }
+  delay(100);
+
 }
+
 
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12); // Configura resolución a 12 bits (0-4095)
-  
+  Wire.begin();  // Inicia I2C
+
   // Configurar pines de ultrasonido
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+
+  // Configurar pines de bombas y establecer estados iniciales
+  pinMode(PIN_BOMBA_PPAL, OUTPUT);
+  bombaPpalEstado = true;  // Bomba principal ON por defecto
+  digitalWrite(PIN_BOMBA_PPAL, HIGH);
   
-  // Configurar pines de control
-  pinMode(PIN_TRASMITIR, INPUT_PULLUP);
-  pinMode(PIN_BOMBA, OUTPUT);
+  pinMode(PIN_BOMBA_A, OUTPUT); 
+  bombaAEstado = false;  // Apagada por defecto
+  digitalWrite(PIN_BOMBA_A, LOW);
   
+  pinMode(PIN_BOMBA_B, OUTPUT);
+  bombaBEstado = false;  // Apagada por defecto
+  digitalWrite(PIN_BOMBA_B, LOW);
+  
+  pinMode(PIN_BOMBA_micro, OUTPUT);
+  bombaMicroEstado = false;  // Apagada por defecto
+  digitalWrite(PIN_BOMBA_micro, LOW);
+  
+  pinMode(PIN_BOMBA_FE, OUTPUT);
+  bombaFEEstado = false;  // Apagada por defecto
+  digitalWrite(PIN_BOMBA_FE, LOW);
+  
+  pinMode(PIN_BOMBA_AGUA_RESERVA, OUTPUT);
+  bombaAguaReservaEstado = false;  // Apagada por defecto
+  digitalWrite(PIN_BOMBA_AGUA_RESERVA, LOW);
+  
+  //pinMode(PIN_SERVO_PH, OUTPUT);
+  //servoMotorPhEstado = false;  // Servo en posición inicial
+  //digitalWrite(PIN_SERVO_PH, 0); // TODO: usar librería Servo para control adecuado
+  
+//seteo pull-down a todos los pines de on/off
+  for (int i = 0; i < n_pines_on_off; i++) {
+    pinMode(pines_on_off[i], INPUT_PULLDOWN);
+  }
+ // instancio e inicio los sensores
   dht.begin(); 
   sensors.begin(); 
 
@@ -88,135 +149,135 @@ void setup() {
     Serial.println("Conectando a WiFi...");
   }
   Serial.println("Conectado a WiFi");
+  
 }
 
 void loop() {
-
-  // Leer sensores que requieren múltiples mediciones
-  for (int i = 0; i < numIter; i++) {
-    sensors.requestTemperatures();
-    tAguaArray[i] = sensors.getTempCByIndex(0);
-    
-    // Leer humedad y validar NaN
-    float humedad = dht.readHumidity();
-    humedadArray[i] = (isnan(humedad)) ? 0.0 : humedad;
-    
-    // Leer temperatura ambiente y validar NaN
-    float tempAmb = dht.readTemperature();
-    tempAmbArray[i] = (isnan(tempAmb)) ? 0.0 : tempAmb;
-    
-    distanciaArray[i] = leerDistancia();
-  }
-
-  // Leer sensores digitales del multiplexor (C0-C7: valores únicos HIGH/LOW)
-  hhEstado = leerSensorDigitalMUX(0);        // C0
-  hEstado = leerSensorDigitalMUX(1);         // C1
-  lEstado = leerSensorDigitalMUX(2);         // C2
-  llEstado = leerSensorDigitalMUX(3);        // C3
-  hhxEstado = leerSensorDigitalMUX(4);       // C4
-  llxEstado = leerSensorDigitalMUX(5);       // C5
-  x1Estado = leerSensorDigitalMUX(6);        // C6
-  x2Estado = leerSensorDigitalMUX(7);        // C7
-
-  // Leer sensores analógicos que requieren múltiples mediciones (C8-C9)
-  leerSensorAnalogicoMultiple(8, phArray, numIter);             // C8 - pH
-  leerSensorAnalogicoMultiple(9, conductividadArray, numIter);  // C9 - Conductividad
-
-
-  // Leer estados de pines digitales
-  byte bombaEstado = digitalRead(PIN_BOMBA);
-  byte transmitirEstado = digitalRead(PIN_TRASMITIR);
-  
-  // Debug - mostrar valores
-  Serial.println("=== SENSORES ===");
-  Serial.print("Temperatura agua: "); Serial.println(tAguaArray[0]);
-  Serial.print("Humedad: "); Serial.println(humedadArray[0]);
-  Serial.print("Temperatura ambiente: "); Serial.println(tempAmbArray[0]);
-  Serial.print("Distancia: "); Serial.println(distanciaArray[0]);
-  Serial.print("HH: "); Serial.println(hhEstado ? "HIGH" : "LOW");
-  Serial.print("H: "); Serial.println(hEstado ? "HIGH" : "LOW");
-  Serial.print("L: "); Serial.println(lEstado ? "HIGH" : "LOW");
-  Serial.print("LL: "); Serial.println(llEstado ? "HIGH" : "LOW");
-  Serial.print("HHX: "); Serial.println(hhxEstado ? "HIGH" : "LOW");
-  Serial.print("LLX: "); Serial.println(llxEstado ? "HIGH" : "LOW");
-  Serial.print("X1: "); Serial.println(x1Estado ? "HIGH" : "LOW");
-  Serial.print("X2: "); Serial.println(x2Estado ? "HIGH" : "LOW");
-  Serial.print("TDS: "); Serial.println(conductividadArray[0]);
-  Serial.print("pH: "); Serial.println(phArray[0]);
-  Serial.print("Bomba: "); Serial.println(bombaEstado);
-  Serial.print("Transmitir: "); Serial.println(transmitirEstado);
-  Serial.println("=== FIN ===");
-
-
-  // Creo un objeto JSON
-  StaticJsonDocument<2000> jsonDoc; 
-  
-  // Arrays para sensores con múltiples mediciones
-  JsonArray tAguaJson = jsonDoc.createNestedArray("tAgua");
-  JsonArray humedadJson = jsonDoc.createNestedArray("humedad");
-  JsonArray tempAmbJson = jsonDoc.createNestedArray("tempAmb");
-  JsonArray distanciaJson = jsonDoc.createNestedArray("distancia");
-  JsonArray tdsJson = jsonDoc.createNestedArray("tds");
-  JsonArray phJson = jsonDoc.createNestedArray("phs");
-
-  // Relleno los arrays JSON con los datos de múltiples mediciones
-  for (int i = 0; i < numIter; i++) {
-    tAguaJson.add(tAguaArray[i]);
-    humedadJson.add(humedadArray[i]);
-    tempAmbJson.add(tempAmbArray[i]);
-    distanciaJson.add(distanciaArray[i]);
-    tdsJson.add(conductividadArray[i]);
-    phJson.add(phArray[i]);
-  }
-
-  // Valores únicos de sensores digitales del multiplexor (C0-C7)
-  jsonDoc["hh"] = hhEstado;
-  jsonDoc["h"] = hEstado;
-  jsonDoc["l"] = lEstado;
-  jsonDoc["ll"] = llEstado;
-  jsonDoc["hhx"] = hhxEstado;
-  jsonDoc["llx"] = llxEstado;
-  jsonDoc["x1"] = x1Estado;
-  jsonDoc["x2"] = x2Estado;
-
-  // Estados de pines digitales
-  jsonDoc["bomba"] = bombaEstado;
-
-  // TODO LO QUE VIENE ABAJO LO HAGO SI EL PIN_TRASMITIR ESTA HIGH
-  if (transmitirEstado == HIGH) {
-  // Convierto el objeto JSON a una cadena
-  String jsonString;
-  serializeJson(jsonDoc, jsonString);
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(serverName);
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(jsonString);
-
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println(httpResponseCode);
-      Serial.println(response);
-    } else {
-      Serial.print("Error en la solicitud POST: ");
-      Serial.println(httpResponseCode);
+    // Leer sensores que requieren múltiples mediciones
+    for (int i = 0; i < numIter; i++) {
+      sensors.requestTemperatures();
+      tAguaArray[i] = sensors.getTempCByIndex(0);
+      
+      // Leer humedad y validar NaN
+      float humedad = dht.readHumidity();
+      humedadArray[i] = (isnan(humedad)) ? 0.0 : humedad;
+      
+      // Leer temperatura ambiente y validar NaN
+      float tempAmb = dht.readTemperature();
+      tempAmbArray[i] = (isnan(tempAmb)) ? 0.0 : tempAmb;
+      
+      distanciaArray[i] = leerAngulo();
+      conductividadArray[i] = analogRead(PIN_EC); // leo la conductividad
+      phArray[i] = analogRead(PIN_PH); // leo el ph
     }
 
-    http.end();
-  } else {
-    Serial.println("WiFi desconectado! reconectando...");
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(3000);
-      Serial.println("Reconectando a WiFi...");
-    }
-    Serial.println("WiFi reconectado");
-  }
-  delay(2000);
-  }
-  else{
-    Serial.println("PIN_TRASMITIR no esta HIGH");
-    delay(200);
-  }
+    // Creo un objeto JSON
+    StaticJsonDocument<2000> jsonDoc; 
+    // Arrays para sensores con múltiples mediciones
+    JsonArray tAguaJson = jsonDoc.createNestedArray("tAgua");
+    JsonArray humedadJson = jsonDoc.createNestedArray("humedad");
+    JsonArray tempAmbJson = jsonDoc.createNestedArray("tempAmb");
+    JsonArray distanciaJson = jsonDoc.createNestedArray("distancia");
+    JsonArray tdsJson = jsonDoc.createNestedArray("tds");
+    JsonArray phJson = jsonDoc.createNestedArray("phs");
 
-}
+    // Relleno los arrays JSON con los datos de múltiples mediciones
+    for (int i = 0; i < numIter; i++) {
+      tAguaJson.add(tAguaArray[i]);
+      humedadJson.add(humedadArray[i]);
+      tempAmbJson.add(tempAmbArray[i]);
+      distanciaJson.add(distanciaArray[i]);
+      tdsJson.add(conductividadArray[i]);
+      phJson.add(phArray[i]);
+    }
+
+    // Leer estado del pin de transmisión (entrada)
+    byte transmitirEstado = digitalRead(PIN_TRASMITIR);
+    
+    //leo los estados de los pines de on/off de nivel
+    byte hhEstado = digitalRead(HH);
+    byte hEstado = digitalRead(H);
+    byte lEstado = digitalRead(L);
+    byte llEstado = digitalRead(LL);
+    byte hh_2Estado = digitalRead(HH_2);
+    byte ll_2Estado = digitalRead(LL_2);
+    
+    // Debug - mostrar valores
+    Serial.println("=== SENSORES ===");
+    Serial.print("Temperatura agua: "); Serial.println(tAguaArray[0]);
+    Serial.print("Humedad: "); Serial.println(humedadArray[0]);
+    Serial.print("Temperatura ambiente: "); Serial.println(tempAmbArray[0]);
+    Serial.print("Distancia: "); Serial.println(distanciaArray[0]);
+    Serial.print("HH: "); Serial.println(hhEstado ? "encontacto" : "libre");
+    Serial.print("H: "); Serial.println(hEstado ? "encontacto" : "libre");
+    Serial.print("L: "); Serial.println(lEstado ? "encontacto" : "libre");
+    Serial.print("LL: "); Serial.println(llEstado ? "encontacto" : "libre");
+    Serial.print("HH_2: "); Serial.println(hh_2Estado ? "encontacto" : "libre");
+    Serial.print("LL_2: "); Serial.println(ll_2Estado ? "encontacto" : "libre");
+    Serial.print("TDS: "); Serial.println(conductividadArray[0]);
+    Serial.print("pH: "); Serial.println(phArray[0]);
+    Serial.print("Bomba ppal: "); Serial.println(bombaPpalEstado ? "ON" : "OFF");
+    Serial.print("Bomba A: "); Serial.println(bombaAEstado ? "ON" : "OFF");
+    Serial.print("Bomba B: "); Serial.println(bombaBEstado ? "ON" : "OFF");
+    Serial.print("Bomba micro: "); Serial.println(bombaMicroEstado ? "ON" : "OFF");
+    Serial.print("Bomba FE: "); Serial.println(bombaFEEstado ? "ON" : "OFF");
+    Serial.print("Bomba agua reserva: "); Serial.println(bombaAguaReservaEstado ? "ON" : "OFF");
+    Serial.print("Servo PH: "); Serial.println(servoMotorPhEstado ? "ON" : "OFF");
+    Serial.print("Transmitir: "); Serial.println(transmitirEstado);
+    Serial.println("=== FIN ===");
+
+
+
+    // Valores únicos de sensores digitales del multiplexor (C0-C7)
+    jsonDoc["HH"] = hhEstado;
+    jsonDoc["H"] = hEstado;
+    jsonDoc["L"] = lEstado;
+    jsonDoc["LL"] = llEstado;
+    jsonDoc["HH_2"] = hh_2Estado;
+    jsonDoc["LL_2"] = ll_2Estado;
+
+    // Estados de pines digitales de bombas y otros
+    jsonDoc["bomba_ppal"] = bombaPpalEstado;
+    jsonDoc["bombaA"] = bombaAEstado;
+    jsonDoc["bombaB"] = bombaBEstado;
+    jsonDoc["bombaMicro"] = bombaMicroEstado;
+    jsonDoc["bombaFE"] = bombaFEEstado;
+    jsonDoc["bombaAguaReserva"] = bombaAguaReservaEstado;
+    jsonDoc["servoMotorPh"] = servoMotorPhEstado;
+
+    // TODO LO QUE VIENE ABAJO LO HAGO SI EL PIN_TRASMITIR ESTA HIGH
+    if (transmitirEstado == HIGH) {
+      // Convierto el objeto JSON a una cadena
+      String jsonString;
+      serializeJson(jsonDoc, jsonString);
+      if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+        http.begin(serverName);
+        http.addHeader("Content-Type", "application/json");
+        int httpResponseCode = http.POST(jsonString);
+
+        if (httpResponseCode > 0) {
+          String response = http.getString();
+          Serial.println(httpResponseCode);
+          Serial.println(response);
+        } else {
+          Serial.print("Error en la solicitud POST: ");
+          Serial.println(httpResponseCode);
+        }
+
+        http.end();
+      } else {
+        Serial.println("WiFi desconectado! reconectando...");
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+          delay(3000);
+          Serial.println("Reconectando a WiFi...");
+        }
+        Serial.println("WiFi reconectado");
+      }
+      delay(1000);
+    }
+    else{
+      Serial.println("PIN_TRASMITIR NO HABILITA LA TRANSMISION DE DATOS");
+      delay(200);
+    }}
